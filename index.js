c0172e1d2b7a89c9f6be7cf0e88d9295c3241db8
Mario2044 <116796406+Mario2044@users.noreply.github.com>
2023-02-06 14:08:12 +0200

    fix

--- index.js
+++ index.js
@@ -1,15 +1,11 @@
 //Реалізуйте функцію removeElement(array, item), щоб видалити елемент item з масиву array.
 const myArray = [1, 2, 3, 4, 5, 6, 7];
 
-function removeValue(value, index, arr) {
-  if (value === 5) {
-    arr.splice(index, 1);
-    return true;
+function removeElement(arr, item) {
+  for (let i = arr.length; i--;) {
+    if (arr[i] === item) arr.splice(i, 1);
   }
-  return false;
+  console.log(arr, item);
 }
 
-const x = myArray.filter(removeValue);
-
-console.log(`myArray values: ${myArray}`);
-console.log(`variable x value: ${x}`);
+removeElement(myArray, 4);
